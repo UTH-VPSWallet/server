8d93aa6d0ab01e310fb30508904dc1b5858f6e93
DESKTOP-CKMKSSM\quyen <quyennt404704>
2026-04-04 15:22:18 +0700

    feat : add model vps +1

--- src/entities/vps.entity.ts
+++ src/entities/vps.entity.ts
@@ -3,6 +3,10 @@ import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 export const VPSEntity = sqliteTable('tbl__VPS', {
     ID: integer('ID').primaryKey(),
     Name: text("Name").notNull(),
+    CPU: text("CPU").notNull(),
+    RAM: text("RAM").notNull(),
+    Storage: text("Storage").notNull(),
+    PricePerMonth: integer("PricePerMonth").notNull(),
     CategoryID: integer("CategoryID").notNull(),
     Status: integer("Status").notNull()
 });
