5959942b03a60bbcb7b29cc127db8fdc674d2429
Araii72 <luonggiahoat@gmail.com>
2026-04-09 17:17:54 +0700

    remove category feature

--- src/entities/category.entity.ts
+++
@@ -1,8 +0,0 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
-
-export const CategoryEntity = sqliteTable('tbl__Category', {
-    ID: integer('ID').primaryKey(),
-    Name: text("Name").notNull(),
-    SupplierEmail: text("SupplierEmail").notNull(),
-    Status: integer("Status").notNull()
-});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { SupplierController } from './controllers/supplier.controller';
-import { CategoryController } from './controllers/category.controller';
 import { VPSController } from "./controllers/vps.controller";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
@@ -17,7 +16,6 @@ app.use('*', cors({
 }));
 
 app.route('/supplier', SupplierController);
-app.route('/category', CategoryController);
 app.route('/vps', VPSController);
 
 export default app;
