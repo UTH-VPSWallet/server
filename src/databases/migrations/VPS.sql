c4ee4f88b2bf7ec404762b1b8cd5663edd0c9983
nguyenhohoangvy <nguyenhohoangvy@gmail.com>
2026-04-10 08:28:54 +0700

    .

--- src/databases/migrations/VPS.sql
+++ src/databases/migrations/VPS.sql
@@ -9,8 +9,9 @@ CREATE TABLE IF NOT EXISTS tbl__VPS (
     RAM TEXT NOT NULL,
     Storage TEXT NOT NULL,
     PricePerMonth INTEGER NOT NULL,
-    Status INTEGER NOT NULL
+    Status INTEGER NOT NULL,
+    Email TEXT NOT NULL
 );
 --VPS Value
-INSERT INTO tbl__VPS (ID, Name, CategoryID,CPU, RAM, Storage, PricePerMonth, Status)
-VALUES (1, 'CHIP01', 1,'3 Core Xeon','2GB','20GB',150000, 1)
+INSERT INTO tbl__VPS (ID, Name, CategoryID,CPU, RAM, Storage, PricePerMonth, Status, Email)
+VALUES (1, 'CHIP01', 1,'3 Core Xeon','2GB','20GB',150000, 1, nhanhoa@gmail.com)
