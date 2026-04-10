8623c1362cd29faf82f7568e2b0f4dd67ee58636
nguyenhohoangvy <nguyenhohoangvy@gmail.com>
2026-04-10 08:40:55 +0700

    .

--- src/databases/migrations/VPS.sql
+++ src/databases/migrations/VPS.sql
@@ -4,7 +4,6 @@ DROP TABLE IF EXISTS tbl__VPS;
 CREATE TABLE IF NOT EXISTS tbl__VPS (
     ID INTEGER PRIMARY KEY,
     Name TEXT NOT NULL,
-    CategoryID INTEGER NOT NULL,
     CPU TEXT NOT NULL,
     RAM TEXT NOT NULL,
     Storage TEXT NOT NULL,
@@ -13,5 +12,8 @@ CREATE TABLE IF NOT EXISTS tbl__VPS (
     Email TEXT NOT NULL
 );
 --VPS Value
-INSERT INTO tbl__VPS (ID, Name, CategoryID,CPU, RAM, Storage, PricePerMonth, Status, Email)
-VALUES (1, 'CHIP01', 1,'3 Core Xeon','2GB','20GB',150000, 1, 'nhanhoa@gmail.com')
+INSERT INTO tbl__VPS (ID, Name, CPU, RAM, Storage, PricePerMonth, Status, Email)
+VALUES  (1, 'CHIP01', '1 Core Xeon', '2GB','20GB', 50000, 1, 'nhanhoa@gmail.com'),
+        (2, 'CHIP02', '2 Core Xeon', '4GB','40GB', 100000, 1, 'nhanhoa@gmail.com'),
+        (3, 'CHIP03', '3 Core Xeon', '6GB','60GB', 150000, 1, 'nhanhoa@gmail.com'),
+        (4, 'CHIP03', '3 Core Xeon', '6GB','60GB', 150000, 1, 'zhost@gmail.com')
