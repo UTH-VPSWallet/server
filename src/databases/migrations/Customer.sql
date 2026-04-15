89f478dec610edd021808d447d94c09634ce00a7
nguyenhohoangvy <nguyenhohoangvy@gmail.com>
2026-04-15 13:13:06 +0700

    .

--- src/databases/migrations/Customer.sql
+++ src/databases/migrations/Customer.sql
@@ -2,9 +2,10 @@
 CREATE TABLE IF NOT EXISTS tbl__Customer (
     Email TEXT PRIMARY KEY,
     Pass TEXT NOT NULL,
+    Phone TEXT NOT NULL,
     Name TEXT NOT NULL,
     Status INTEGER NOT NULL
 );
 --Customer Value
-INSERT INTO tbl__Customer (Email, Pass, Name, Status)
-VALUES ('vy@gmail.com', 1111, 'Vỹ', 1);
+INSERT INTO tbl__Customer (Email, Pass, Phone, Name, Status)
+VALUES ('vy@gmail.com', '$2b$10$lZ.m7l67a/dCx48/Fdeqf.c0iry..KGa5p2TQE4bQ4HpXX3SqTz6m', '01218577732', 'Vỹ', 1);
