7a011ba119a56d5eac9dc3eb11eaba69f8b06182
nguyenhohoangvy <nguyenhohoangvy@gmail.com>
2026-04-15 13:16:16 +0700

    .

--- src/entities/customer.entity.ts
+++ src/entities/customer.entity.ts
@@ -3,6 +3,7 @@ import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 export const CustomerEntity = sqliteTable('tbl__Customer', {
   Email: text('Email').primaryKey(),
   Pass: text('Pass').notNull(),
+  Phone: text('Phone').notNull(),
   Name: text('Name').notNull(),
   Status: integer('Status').notNull()
 });
