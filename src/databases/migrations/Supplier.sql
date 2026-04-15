98451b6e2e63ee8c29078775da73062e50729d75
nguyenhohoangvy <nguyenhohoangvy@gmail.com>
2026-04-15 10:12:47 +0700

    .

--- src/databases/migrations/Supplier.sql
+++ src/databases/migrations/Supplier.sql
@@ -11,4 +11,4 @@ CREATE TABLE IF NOT EXISTS tbl__Supplier (
 );
 --Supplier Value
 INSERT INTO tbl__Supplier (Email, Pass, Phone, Name, Location, Status)
-VALUES ('nhanhoa@gmail.com', '$2b$10$lZ.m7l67a/dCx48/Fdeqf.c0iry..KGa5p2TQE4bQ4HpXX3SqTz6m', '01219711231' ,'Nhân Hòa', 'Hà Nội' , 1);
+VALUES ('nhanhoa@gmail.com', '$2b$10$lZ.m7l67a/dCx48/Fdeqf.c0iry..KGa5p2TQE4bQ4HpXX3SqTz6m', '01219711231' ,'Nhân Hòa', 'Hà Nội' , 200);
