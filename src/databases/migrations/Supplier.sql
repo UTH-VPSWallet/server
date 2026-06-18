8d9d97808f8b140f064a0c7e7b08b0eed8d07d3c
nguyenhohoangvy <nguyenhohoangvy@gmail.com>
2026-06-18 12:44:12 +0700

    .

--- src/databases/migrations/Supplier.sql
+++ src/databases/migrations/Supplier.sql
@@ -2,20 +2,20 @@ DROP TABLE IF EXISTS tbl__Supplier;
 
 --Supplier Table
 CREATE TABLE IF NOT EXISTS tbl__Supplier (
-    Email TEXT PRIMARY KEY,
-    Pass TEXT NOT NULL,
-    Logo TEXT NOT NULL,
-    Phone TEXT NOT NULL,
-    Name TEXT NOT NULL,
-    Location TEXT NOT NULL,
-    Status INTEGER NOT NULL
+  Email TEXT PRIMARY KEY,
+  Pass TEXT NOT NULL,
+  Logo TEXT NOT NULL,
+  Phone TEXT NOT NULL,
+  Name TEXT NOT NULL,
+  Location TEXT NOT NULL,
+  Status INTEGER NOT NULL
 );
 --Supplier Value
 INSERT INTO tbl__Supplier (Email, Pass, Logo, Phone, Name, Location, Status)
 VALUES ('nhanhoa@gmail.com', '$2b$10$lZ.m7l67a/dCx48/Fdeqf.c0iry..KGa5p2TQE4bQ4HpXX3SqTz6m', 'nhanhoa.png', '01219711231' ,'Nhân Hòa', 'Hà Nội' , 200),
-('zhost@gmail.com', '$2b$10$lZ.m7l67a/dCx48/Fdeqf.c0iry..KGa5p2TQE4bQ4HpXX3SqTz6m', 'zhost.webp', '01219711232' ,'ZHost', 'Tp.HCM' , 200),
-('vinahost@gmail.com', '$2b$10$lZ.m7l67a/dCx48/Fdeqf.c0iry..KGa5p2TQE4bQ4HpXX3SqTz6m', 'vinahost.png', '01219711233' ,'VinaHost', 'Tp.HCM' , 200),
-('vietnix@gmail.com', '$2b$10$lZ.m7l67a/dCx48/Fdeqf.c0iry..KGa5p2TQE4bQ4HpXX3SqTz6m', 'vietnix.png', '01219711234' ,'VietNix', 'Hà Nội' , 200),
-('bkns@gmail.com', '$2b$10$lZ.m7l67a/dCx48/Fdeqf.c0iry..KGa5p2TQE4bQ4HpXX3SqTz6m', 'bkns.png', '01219711235' ,'Nhân Hòa', 'BKNS' , 200),
-('zonecloud@gmail.com', '$2b$10$lZ.m7l67a/dCx48/Fdeqf.c0iry..KGa5p2TQE4bQ4HpXX3SqTz6m', 'zonecloud.png', '01219711236' ,'ZoneCloud', 'Đà Nẵng' , 200),
-('matbao@gmail.com', '$2b$10$lZ.m7l67a/dCx48/Fdeqf.c0iry..KGa5p2TQE4bQ4HpXX3SqTz6m', 'matbao.svg', '01219711237' ,'Mắc Bão', 'Tp.HCM' , 200);
+       ('zhost@gmail.com', '$2b$10$lZ.m7l67a/dCx48/Fdeqf.c0iry..KGa5p2TQE4bQ4HpXX3SqTz6m', 'zhost.webp', '01219711232' ,'ZHost', 'Tp.HCM' , 200),
+       ('vinahost@gmail.com', '$2b$10$lZ.m7l67a/dCx48/Fdeqf.c0iry..KGa5p2TQE4bQ4HpXX3SqTz6m', 'vinahost.png', '01219711233' ,'VinaHost', 'Tp.HCM' , 200),
+       ('vietnix@gmail.com', '$2b$10$lZ.m7l67a/dCx48/Fdeqf.c0iry..KGa5p2TQE4bQ4HpXX3SqTz6m', 'vietnix.png', '01219711234' ,'VietNix', 'Hà Nội' , 200),
+       ('bkns@gmail.com', '$2b$10$lZ.m7l67a/dCx48/Fdeqf.c0iry..KGa5p2TQE4bQ4HpXX3SqTz6m', 'bkns.png', '01219711235' ,'BKSN', 'Hà Nội' , 200),
+       ('zonecloud@gmail.com', '$2b$10$lZ.m7l67a/dCx48/Fdeqf.c0iry..KGa5p2TQE4bQ4HpXX3SqTz6m', 'zonecloud.png', '01219711236' ,'ZoneCloud', 'Đà Nẵng' , 200),
+       ('matbao@gmail.com', '$2b$10$lZ.m7l67a/dCx48/Fdeqf.c0iry..KGa5p2TQE4bQ4HpXX3SqTz6m', 'matbao.svg', '01219711237' ,'Mắc Bão', 'Tp.HCM' , 200);
--- src/databases/migrations/VPS.sql
+++ src/databases/migrations/VPS.sql
@@ -2,18 +2,60 @@ DROP TABLE IF EXISTS tbl__VPS;
 
 --VPS Table
 CREATE TABLE IF NOT EXISTS tbl__VPS (
-    ID INTEGER PRIMARY KEY,
-    Name TEXT NOT NULL,
-    CPU TEXT NOT NULL,
-    RAM TEXT NOT NULL,
-    Storage TEXT NOT NULL,
-    PricePerMonth INTEGER NOT NULL,
-    Status INTEGER NOT NULL,
-    Email TEXT NOT NULL
+  ID INTEGER PRIMARY KEY,
+  Name TEXT NOT NULL,
+  CPU TEXT NOT NULL,
+  RAM TEXT NOT NULL,
+  Storage TEXT NOT NULL,
+  PricePerMonth INTEGER NOT NULL,
+  Category INTEGER NOT NULL,
+  Status INTEGER NOT NULL,
+  Email TEXT NOT NULL
 );
 --VPS Value
-INSERT INTO tbl__VPS (ID, Name, CPU, RAM, Storage, PricePerMonth, Status, Email)
-VALUES  (1, 'CHIP01', '1 Core Xeon', '2GB','20GB', 50000, 1, 'nhanhoa@gmail.com'),
-        (2, 'CHIP02', '2 Core Xeon', '4GB','40GB', 100000, 1, 'nhanhoa@gmail.com'),
-        (3, 'CHIP03', '3 Core Xeon', '6GB','60GB', 150000, 1, 'nhanhoa@gmail.com'),
-        (4, 'CHIP03', '3 Core Xeon', '6GB','60GB', 150000, 1, 'zhost@gmail.com')
+INSERT INTO tbl__VPS (ID, Name, CPU, RAM, Storage, PricePerMonth, Category, Status, Email)
+VALUES (1, 'CHIP01', '1 Core Xeon', '2GB', '20GB', 50000, 1, 1, 'nhanhoa@gmail.com'),
+       (2, 'CHIP02', '2 Core Xeon', '4GB', '40GB', 100000, 1, 1, 'nhanhoa@gmail.com'),
+       (3, 'SSD01', '2 Core Xeon', '4GB', '100GB', 150000, 2, 1, 'nhanhoa@gmail.com'),
+       (4, 'SSD02', '4 Core Xeon', '8GB', '500GB', 250000, 2, 1, 'nhanhoa@gmail.com'),
+       (5, 'RAM01', '2 Core Xeon', '16GB', '20GB', 200000, 3, 1, 'nhanhoa@gmail.com'),
+       (6, 'RAM02', '4 Core Xeon', '32GB', '40GB', 350000, 3, 1, 'nhanhoa@gmail.com'),
+       (7, 'CHIP01', '1 Core Xeon', '2GB', '20GB', 50000, 1, 1, 'zhost@gmail.com'),
+       (8, 'CHIP02', '2 Core Xeon', '4GB', '40GB', 100000, 1, 1, 'zhost@gmail.com'),
+       (9, 'SSD01', '2 Core Xeon', '4GB', '120GB', 170000, 2, 1, 'zhost@gmail.com'),
+       (10, 'SSD02', '4 Core Xeon', '8GB', '350GB', 200000, 2, 1, 'zhost@gmail.com'),
+       (11, 'RAM01', '2 Core Xeon', '16GB', '20GB', 200000, 3, 1, 'zhost@gmail.com'),
+       (12, 'RAM02', '4 Core Xeon', '32GB', '40GB', 350000, 3, 1, 'zhost@gmail.com'),
+       (13, 'CHIP01', '1 Core Xeon', '2GB', '20GB', 50000, 1, 1, 'vinahost@gmail.com'),
+       (14, 'CHIP02', '2 Core Xeon', '4GB', '40GB', 100000, 1, 1, 'vinahost@gmail.com'),
+       (15, 'SSD01', '2 Core Xeon', '4GB', '120GB', 170000, 2, 1, 'vinahost@gmail.com'),
+       (16, 'SSD02', '4 Core Xeon', '8GB', '350GB', 200000, 2, 1, 'vinahost@gmail.com'),
+       (17, 'RAM01', '2 Core Xeon', '16GB', '20GB', 200000, 3, 1, 'vinahost@gmail.com'),
+       (18, 'RAM02', '4 Core Xeon', '32GB', '40GB', 350000, 3, 1, 'vinahost@gmail.com'),
+       (19, 'CHIP01', '1 Core Xeon', '2GB', '20GB', 50000, 1, 1, 'vietnix@gmail.com'),
+       (20, 'CHIP02', '2 Core Xeon', '4GB', '40GB', 100000, 1, 1, 'vietnix@gmail.com'),
+       (21, 'SSD01', '2 Core Xeon', '4GB', '120GB', 170000, 2, 1, 'vietnix@gmail.com'),
+       (22, 'SSD02', '4 Core Xeon', '8GB', '350GB', 200000, 2, 1, 'vietnix@gmail.com'),
+       (23, 'RAM01', '2 Core Xeon', '16GB', '20GB', 200000, 3, 1, 'vietnix@gmail.com'),
+       (24, 'RAM02', '4 Core Xeon', '32GB', '40GB', 350000, 3, 1, 'vietnix@gmail.com'),
+       (25, 'CHIP01', '1 Core Xeon', '2GB', '20GB', 50000, 1, 1, 'bkns@gmail.com'),
+       (26, 'CHIP02', '2 Core Xeon', '4GB', '40GB', 100000, 1, 1, 'bkns@gmail.com'),
+       (27, 'SSD01', '2 Core Xeon', '4GB', '120GB', 170000, 2, 1, 'bkns@gmail.com'),
+       (28, 'SSD02', '4 Core Xeon', '8GB', '350GB', 200000, 2, 1, 'bkns@gmail.com'),
+       (29, 'RAM01', '2 Core Xeon', '16GB', '20GB', 200000, 3, 1, 'bkns@gmail.com'),
+       (30, 'RAM02', '4 Core Xeon', '32GB', '40GB', 350000, 3, 1, 'bkns@gmail.com'),
+       (31, 'CHIP01', '1 Core Xeon', '2GB', '20GB', 50000, 1, 1, 'zonecloud@gmail.com'),
+       (32, 'CHIP02', '2 Core Xeon', '4GB', '40GB', 100000, 1, 1, 'zonecloud@gmail.com'),
+       (33, 'SSD01', '2 Core Xeon', '4GB', '120GB', 170000, 2, 1, 'zonecloud@gmail.com'),
+       (34, 'SSD02', '4 Core Xeon', '8GB', '350GB', 200000, 2, 1, 'zonecloud@gmail.com'),
+       (35, 'RAM01', '2 Core Xeon', '16GB', '20GB', 200000, 3, 1, 'zonecloud@gmail.com'),
+       (36, 'RAM02', '4 Core Xeon', '32GB', '40GB', 350000, 3, 1, 'zonecloud@gmail.com'),
+       (37, 'CHIP01', '1 Core Xeon', '2GB', '20GB', 50000, 1, 1, 'matbao@gmail.com'),
+       (38, 'CHIP02', '2 Core Xeon', '4GB', '40GB', 100000, 1, 1, 'matbao@gmail.com'),
+       (39, 'SSD01', '2 Core Xeon', '4GB', '120GB', 170000, 2, 1, 'matbao@gmail.com'),
+       (40, 'SSD02', '4 Core Xeon', '8GB', '350GB', 200000, 2, 1, 'matbao@gmail.com'),
+       (41, 'RAM01', '2 Core Xeon', '16GB', '20GB', 200000, 3, 1, 'matbao@gmail.com'),
+       (42, 'RAM02', '4 Core Xeon', '32GB', '40GB', 350000, 3, 1, 'matbao@gmail.com');
+
+
+       
